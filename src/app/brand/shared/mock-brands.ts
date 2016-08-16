8104f3d3e4974c1b7d43fb3a7f04611b0807c5fa
Matthew Hall <texaskiwi@live.com>
2016-08-16 16:25:26 -0500

    Side nav and sentiment offset
    Side nav and sentiment offset added.

--- src/app/brand/shared/mock-brands.ts
+++ src/app/brand/shared/mock-brands.ts
@@ -2,7 +2,7 @@ import {Brand} from './brand.model'
 
 
 export var BRANDS: Brand[] = [
-    {"id":1, "title": "BMW","description":"","iconUrl":"http://www.bmw.com","webHome":"http://www.bmw.com","sentiment":0.5},
+    {"id":1, "title": "BMW","description":"","iconUrl":"http://www.bmw.com","webHome":"http://www.bmw.com","sentiment":0},
     {"id":2, "title": "Mercedes-Benz","description":"","iconUrl":"","webHome":"","sentiment":-0.5},
     {"id":3, "title": "Volkswagen","description":"","iconUrl":"","webHome":"","sentiment":-0.3},
     {"id":4, "title": "Toyota","description":"","iconUrl":"","webHome":"","sentiment":0.5},
@@ -11,6 +11,6 @@ export var BRANDS: Brand[] = [
     {"id":7, "title": "Subaru","description":"","iconUrl":"","webHome":"","sentiment":1},
     {"id":8, "title": "Tesla","description":"","iconUrl":"","webHome":"","sentiment":0},
     {"id":9, "title": "Chevrolet","description":"","iconUrl":"","webHome":"","sentiment":-1},
-    {"id":10, "title": "Ford","description":"","iconUrl":"","webHome":"","sentiment":0.7},
-    {"id":11, "title": "Chrysler","description":"","iconUrl":"","webHome":"","sentiment":0.8}
+    {"id":10, "title": "Ford","description":"","iconUrl":"","webHome":"","sentiment":0.99},
+    {"id":11, "title": "Chrysler","description":"","iconUrl":"","webHome":"","sentiment":1}
  ];
--- src/app/sentiment-summary/sentiment-summary.component.ts
+++ src/app/sentiment-summary/sentiment-summary.component.ts
@@ -20,6 +20,33 @@ export class SentimentSummaryComponent implements OnInit {
   }
 
 
+  public get sentimentPosition():string
+  {
+//  points="0,10, 20,10, 10,0" 
+    let ans:string = "";
+
+     if(this.selectedSentimentObject != null)
+     {
+            let offset:number = this.selectedSentimentObject.sentiment;
+            if(offset <=0)
+            {
+              offset = offset * -2.5;
+            }
+            else{
+              offset = offset * -22;
+            }
+
+            
+
+            let x1 = 0 + offset;
+            let x2 = 20 + offset;
+            let x3 =  10 + offset;
+            ans = x1 + `,10,` + x2 + `,10,` + x3 + `,0`;
+     }
+
+     return ans;
+  }
+
   public get sentimentPercentage():number
     {
           let value:number = this.selectedSentimentObject.sentiment;
